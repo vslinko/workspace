f1c256a2c0c02aebbb3b71bbb27946f197f76e9e
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2013-08-21 11:47:07 +0400

    feature: move submodules to ignored directory

--- commit_submodules.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd $(dirname $0)
-submodules=$(grep 'path =' .gitmodules | awk '{print $3}')
-for submodule in $submodules; do
-  git add $submodule
-done
-git commit $submodules -m "chore: submodules update"
